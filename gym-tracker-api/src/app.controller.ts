b1aef7b516e538975fd318d134fcf5bffd1babd2
Le Tran Anh Vu <vule5726@gmail.com>
2024-06-16 18:35:18 +0300

    Setup drizzle and have some example code

--- gym-tracker-api/src/app.controller.ts
+++ gym-tracker-api/src/app.controller.ts
@@ -1,12 +1,40 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    HttpCode,
+    HttpStatus,
+    Param,
+    ParseIntPipe,
+    Post
+} from '@nestjs/common'
 
-@Controller()
+import { TodoService } from './todo.service'
+import { InsertTodo } from './drizzle/schema/schema'
+
+@Controller('todos')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+    constructor(private readonly todoService: TodoService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+    @Get()
+    getAll() {
+        return this.todoService.findAll()
+    }
+
+    @Get(':id')
+    getOne(@Param('id', ParseIntPipe) id: number) {
+        return this.todoService.findOne(id)
+    }
+
+    @Post()
+    create(@Body() dto: InsertTodo) {
+        return this.todoService.create(dto)
+    }
+
+    @Delete(':id')
+    @HttpCode(HttpStatus.NO_CONTENT)
+    delete(@Param('id', ParseIntPipe) id: number) {
+        return this.todoService.delete(id)
+    }
 }
--- gym-tracker-api/src/app.module.ts
+++ gym-tracker-api/src/app.module.ts
@@ -1,14 +1,17 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { ConfigModule } from '@nestjs/config';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { ConfigModule } from '@nestjs/config'
+import { TodoService } from './todo.service'
+import { DrizzleModule } from './drizzle/drizzle.module'
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      envFilePath: ['.env.local', '.env'],
-    }),
-  ],
-  controllers: [AppController],
-  providers: [],
+    imports: [
+        ConfigModule.forRoot({
+            envFilePath: ['.env.local', '.env']
+        }),
+        DrizzleModule
+    ],
+    controllers: [AppController],
+    providers: [TodoService]
 })
 export class AppModule {}
